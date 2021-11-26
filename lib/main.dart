63d1b6a73b4e837bdb0c7a5e622df3a2b50ac70c
Luciano <luciano01.dev@gmail.com>
2021-11-25 22:32:44 -0300

    Fixing conflicting by merge.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:flutter_modular/flutter_modular.dart';
-
-import 'app/app_module.dart';
-import 'app/app_widget.dart';
-
-void main() => runApp(ModularApp(module: AppModule(), child: AppWidget()));
-=======
 import 'package:lazy_loading/app_widget.dart';
 
 void main() => runApp(AppWidget());
->>>>>>> feature-bloc
